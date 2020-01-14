346f0db67f4558af7080b4841ae5468e9fbdce63
Maxtream <max.orlovsky@gmail.com>
2020-01-14 22:07:45 +0100

    feat(avatar): add avatar component responsible for visual of character

---
+++ src/components/avatar/avatar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="{'avatar--just-head': justHead}"
+        class="avatar"
+    >
+        <div class="avatar__body">
+            <img :src="`/dist/assets/images/bodies/novice_${gender}_0.png`">
+        </div>
+        <div :class="[`avatar__head__${headStyle}`, gender]"
+            class="avatar__head"
+        >
+            <img :src="`/dist/assets/images/heads/${headStyle}_${gender}.gif`">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'avatar',
+    props: {
+        headStyle: {
+            type: Number,
+            required: true
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        justHead: {
+            type: Boolean,
+            default: false
+        }
+    }
+};
+</script>
