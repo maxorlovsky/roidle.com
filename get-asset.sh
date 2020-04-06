761c9a31e1f67ed56ca837b13f9d1045e411a28f
Maxtream <max.orlovsky@gmail.com>
2020-04-06 23:10:35 +0200

    chore(assets-items): add script to fast get assets from RMS

---
+++ get-asset.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -O ./assets/images/items/large/$1.gif http://file5.ratemyserver.net/items/large/$1.gif
+wget -O ./assets/images/items/$1.gif http://file5.ratemyserver.net/items/small/$1.gif
