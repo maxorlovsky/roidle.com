38a8979360b6092c12a207983c6f6177542b1c1f
Maxtream <max.orlovsky@gmail.com>
2020-06-23 22:53:02 +0200

    feat(hunt-actions): add active skills support

--- src/components/hunt-actions/hunt-actions.vue
+++ src/components/hunt-actions/hunt-actions.vue
@@ -69,16 +69,30 @@
                 <div class="healing-macro">
                     <p>Healing when</p>
                     <select v-model="healingWhen">
-                        <option value="10">&lt; 10HP</option>
-                        <option value="20">&lt; 20HP</option>
-                        <option value="30">&lt; 30HP</option>
-                        <option value="40">&lt; 40HP</option>
-                        <option value="50">&lt; 50HP</option>
-                        <option value="60">&lt; 60HP</option>
-                        <option value="70">&lt; 70HP</option>
-                        <option value="80">&lt; 80HP</option>
-                        <option value="90">&lt; 90HP</option>
+                        <option value="10">&lt; 10% HP</option>
+                        <option value="20">&lt; 20% HP</option>
+                        <option value="30">&lt; 30% HP</option>
+                        <option value="40">&lt; 40% HP</option>
+                        <option value="50">&lt; 50% HP</option>
+                        <option value="60">&lt; 60% HP</option>
+                        <option value="70">&lt; 70% HP</option>
+                        <option value="80">&lt; 80% HP</option>
+                        <option value="90">&lt; 90% HP</option>
                     </select>
+                </div>
+
+                <p>Active skills</p>
+                <div class="active-skills">
+                    <div v-for="(item, index) in activeSkills"
+                        :key="index"
+                        class="active-skills__item"
+                        @click="chooseHuntActiveSkills(index)"
+                    >
+                        <template v-if="item">
+                            <img :src="`/dist/assets/images/skills/${item.image}`">
+                            <span class="active-skills__item__amount">{{ item.mpControl }}%</span>
+                        </template>
+                    </div>
                 </div>
             </div>
             <div class="modal__buttons">
@@ -90,6 +104,68 @@
                     class="btn game-button"
                     @click="startHunt()"
                 >Start</button>
+            </div>
+        </div>
+
+        <div v-if="showActiveSkillsModal"
+            class="active-skill-modal modal"
+        >
+            <div v-if="activeSkills[skillSelectedSlot]"
+                class="active-skill-modal__item"
+                @click="removeActiveSkill(skillSelectedSlot)"
+            >
+                <div class="active-skill-modal__item__image">
+                    <img src="/dist/assets/images/cancel.png">
+                </div>
+                <div>
+                    <div class="active-skill-modal__item__name">Remove skill</div>
+                </div>
+            </div>
+
+            <template v-if="activeSkillsList && activeSkillsList.length">
+                <div v-for="(item, index) in activeSkillsList"
+                    :key="index"
+                    class="active-skill-modal__item"
+                >
+                    <div class="active-skill-modal__item__image"
+                        @click="addActiveSkill(item)"
+                    >
+                        <img :src="`/dist/assets/images/skills/${item.image}`">
+                    </div>
+                    <div class="active-skill-modal__item__macros">
+                        <div class="active-skill-modal__item__name"
+                            @click="addActiveSkill(item)"
+                        >
+                            {{ item.name }} (Lv. {{ item.currentLevel }})
+                        </div>
+                        <div class="active-skill-modal__item__mp-control">
+                            <p>Use while MP is higher than</p>
+                            <select v-model="item.mpControl">
+                                <option value="0">0% MP</option>
+                                <option value="10">10% MP</option>
+                                <option value="20">20% MP</option>
+                                <option value="30">30% MP</option>
+                                <option value="40">40% MP</option>
+                                <option value="50">50% MP</option>
+                                <option value="60">60% MP</option>
+                                <option value="70">70% MP</option>
+                                <option value="80">80% MP</option>
+                                <option value="90">90% MP</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </template>
+            <div v-else
+                class="active-skill-modal__item"
+            >
+                No active skills found
+            </div>
+
+            <div class="modal__buttons active-skill-modal__buttons">
+                <button class="btn btn-secondary"
+                    @click="showActiveSkillsModal = false"
+                >Cancel</button>
             </div>
         </div>
 
@@ -151,11 +227,15 @@ export default {
         return {
             showHuntModal: false,
             showHealingModal: false,
+            showActiveSkillsModal: false,
             huntTime: functions.storage('get', 'huntSelectedTime') || null,
             enableLongerHunt: false,
             huntHealingItems: [null, null, null],
+            activeSkills: [null, null, null, null, null],
             healingItemsList: [],
+            activeSkillsList: [],
             healingSelectedSlot: null,
+            skillSelectedSlot: null,
             healingWhen: functions.storage('get', 'huntHealingWhen') || '10'
         };
     },
@@ -197,11 +277,26 @@ export default {
             this.healingItemsList = response.items;
         });
 
+        mo.socket.on('getSkillsDataComplete', (response) => {
+            // Saving only active skills in the list and adding skill level of this character
+            for (const skill of response) {
+                // If skill have mp requirement - it's active
+                // Also check if skill level is at least 1, otherwise don't add it to the list
+                if (skill.mp && this.characterSkills[skill.id]) {
+                    skill.currentLevel = this.characterSkills[skill.id];
+                    // Add default MP control parameter
+                    skill.mpControl = 0;
+                    this.activeSkillsList.push(skill);
+                }
+            }
+        });
+
         this.checkHuntState();
     },
     beforeDestroy() {
         mo.socket.off('startHuntComplete');
         mo.socket.off('getHealingItemsComplete');
+        mo.socket.off('getSkillsDataComplete');
     },
     methods: {
         checkHuntState() {
@@ -214,19 +309,28 @@ export default {
             this.huntHealingItems = [null, null, null];
             this.showHealingModal = false;
             this.healingItemsList = [];
+            this.activeSkills = [null, null, null, null, null];
+            this.activeSkillsList = [];
 
             // Showing hunt modal
             this.showHuntModal = true;
 
             // Trigger to fetch healing items
             mo.socket.emit('getHealingItems');
+
+            // Trigger to fetch active skills
+            mo.socket.emit('getSkillsData');
         },
         cancelHunt() {
             // Reset all variables and dialogs
             this.huntHealingItems = [null, null, null];
             this.showHealingModal = false;
             this.healingItemsList = [];
+            this.activeSkills = [null, null, null, null, null];
+            this.activeSkillsList = [];
             this.showHuntModal = false;
+            this.showHealingModal = false;
+            this.showActiveSkillsModal = false;
         },
         startHunt() {
             // If fight in progress, we don't start another one
@@ -247,16 +351,36 @@ export default {
                 }
             }
 
+            const skillsToUseInHunt = [];
+
+            for (let i = 0; i <= 4; ++i) {
+                if (this.activeSkills[i]) {
+                    skillsToUseInHunt.push({
+                        skillId: this.activeSkills[i].id,
+                        level: parseInt(this.activeSkills[i].currentLevel),
+                        mpControl: parseInt(this.activeSkills[i].mpControl)
+                    });
+                } else {
+                    // For basic attack
+                    skillsToUseInHunt.push(null);
+                }
+            }
+
             mo.socket.emit('startHunt', {
                 position: 0,
                 time: this.huntTime,
                 itemToUseInHunt: itemToUseInHunt,
+                skillsToUseInHunt: skillsToUseInHunt,
                 healingHp: parseInt(this.healingWhen)
             });
         },
         chooseHuntHealingItem(index) {
             this.showHealingModal = true;
             this.healingSelectedSlot = index;
+        },
+        chooseHuntActiveSkills(index) {
+            this.showActiveSkillsModal = true;
+            this.skillSelectedSlot = index;
         },
         addHealingItem(item) {
             // If items is already in this slot, we first need to return it
@@ -281,6 +405,14 @@ export default {
             // Cleaning up the slot
             this.huntHealingItems[slot] = null;
             this.showHealingModal = false;
+        },
+        addActiveSkill(item) {
+            this.activeSkills[this.skillSelectedSlot] = { ...item };
+            this.showActiveSkillsModal = false;
+        },
+        removeActiveSkill(slot) {
+            this.activeSkills[slot] = null;
+            this.showActiveSkillsModal = false;
         },
         returnHealingItem(slot) {
             // Add item back to healing list items
