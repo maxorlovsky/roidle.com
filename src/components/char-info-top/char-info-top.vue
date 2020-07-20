be0c6016a861f42cf4d9966687404731d25cd7e0
Maxtream <max.orlovsky@gmail.com>
2020-07-20 17:03:57 +0200

    fix(char-info-top): fix interval/timer problem of the hunt

--- src/components/char-info-top/char-info-top.vue
+++ src/components/char-info-top/char-info-top.vue
@@ -206,6 +206,9 @@ export default {
         });
 
         mo.socket.on('stopHuntComplete', () => {
+            this.huntingDisplay = '';
+            clearInterval(this.interval);
+
             this.$store.commit('huntStatus', {
                 status: false,
                 timeFinish: null
