16894b37853b75d72eaac48c58ba0cd12739e5eb
Maxtream <max.orlovsky@gmail.com>
2020-07-26 16:42:43 +0200

    feat(morocc): add maps
    - add bgm
    - add items assets
    - add map images
    - add desert background

--- src/pages/map/map.vue
+++ src/pages/map/map.vue
@@ -7,8 +7,8 @@
             :key="index"
             class="map__row"
         >
-            <div v-for="location in piece"
-                :key="location.id"
+            <div v-for="(location, subIndex) in piece"
+                :key="subIndex"
                 :class="{'map__piece--selected': location.id === characterLocationId, 'map__piece--traveling': location.id === travelingToLocation, 'map__piece--disabled': (travelingToLocation || restInProgress || huntStatus || userOverweight) && location.id !== travelingToLocation}"
                 class="map__piece"
                 @click="selectMap(location.id)"
